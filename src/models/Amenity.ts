e4d605810a0fe7a49030b118d8c423be2ab4109e
ZeenathulIlma <seenathulilma121243@gmail.com>
2025-12-22 18:10:48 +0530

    feat: update Amenity model to make description, category, and iconName optional

--- src/models/Amenity.ts
+++ src/models/Amenity.ts
@@ -3,13 +3,17 @@ import mongoose, { Document, Schema } from "mongoose";
 export interface IAmenity extends Document {
     _id: mongoose.Types.ObjectId,
     amenityname: string,
-    description: string
+    description?: string,
+    category?: string;
+    iconName?: string;
 }
 
 const amenitySchema = new Schema<IAmenity>(
     {
         amenityname: { type: String, required: true, unique: true },
-        description: { type: String, required: true }
+        description: { type: String, required: true },
+        category: { type: String, required: true },
+        iconName: { type: String, required: true }
     },
     {
         timestamps: true
