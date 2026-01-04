7ea9723e01cb098adc95deb2ad3e937bc17cc145
ZeenathulIlma <seenathulilma121243@gmail.com>
2026-01-04 22:24:19 +0530

    Modify CORS Origin

--- src/index.ts
+++ src/index.ts
@@ -37,8 +37,12 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "https://rad-course-work-hoterra-frontend.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
   })
 );
+
+app.options("*", cors());
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/roomtype", roomTypeRouter)
