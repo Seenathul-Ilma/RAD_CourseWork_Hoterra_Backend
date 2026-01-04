11ec7849558abd986934bf73688504e496853f44
ZeenathulIlma <seenathulilma121243@gmail.com>
2026-01-04 22:35:00 +0530

    debugging cors issue

--- src/index.ts
+++ src/index.ts
@@ -29,24 +29,16 @@ const DEFAULT_ADMIN_PHONE = process.env.DEFAULT_ADMIN_PHONE as string
 // Create an Express app instance to handle routes, middleware, and server configurations
 const app = express();
 
+// Register built-in middleware to parse incoming JSON request bodies
+app.use(express.json());
+
 // Enable CORS origin for frontend (including preflight OPTIONS requests)
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://rad-course-work-hoterra-frontend.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+    origin: ["http://localhost:5173", "https://rad-course-work-hoterra-frontend.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE", "PATCH", "OPTIONS"],
   })
 );
-
-// Handle preflight
-app.options("*", cors());
-
-// Register built-in middleware to parse incoming JSON request bodies
-app.use(express.json());
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/roomtype", roomTypeRouter)
