c9639799793ae27f82eb5c9941c94c5d77d17b39
ZeenathulIlma <seenathulilma121243@gmail.com>
2026-01-04 22:31:02 +0530

    debugging cors issue

--- src/index.ts
+++ src/index.ts
@@ -32,16 +32,18 @@ const app = express();
 // Enable CORS origin for frontend (including preflight OPTIONS requests)
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://rad-course-work-hoterra-frontend.vercel.app"],
-    methods: ["POST", "GET", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    origin: [
+      "http://localhost:5173",
+      "https://rad-course-work-hoterra-frontend.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
   })
 );
 
+// Handle preflight
 app.options("*", cors());
-
-app.use(cors());
 
 // Register built-in middleware to parse incoming JSON request bodies
 app.use(express.json());
